75e69ee8402f6df5a67ac63606763284924a00da
Youllou <62644404+Youllou@users.noreply.github.com>
2020-11-23 19:03:23 +0100

    The program will be divided in many parts
    This will be the programm that reunite the whole bot

---
+++ roleBot.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:			roleBot
+# Purpose:		This bot is a discord bot
+#				it's purpose is to help admin of a role play discord server
+#				by adding some commands
+#				This bot is divided in 4 parts (including this one)
+#				This programme is the reunificator of the 3 other and also launch the bot
+#
+# Author:      Youllou
+#
+# Created:     20/11/2020
+# Copyright:   (c) Youllou 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+from deff import *
+from player import *
+
+@roleBot.command()
+async def help(ctx):
+	await ctx.author.create_dm()
+	await ctx.author.send("""roleBot is divided in some parts
+							 Use '!help_player' to have the player's commands
+							 Use'!help_GM' to have the Game master's commands""")
+
+
+roleBot.run("Nzc5NzQ1MTU0MjM5ODIzODkz.X7lAQw.DL8WVuMFTy0cnykZWYfDLuILoVs")
